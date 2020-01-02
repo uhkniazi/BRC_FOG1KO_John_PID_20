1c53aa2a8cc35726db5831c6aa3d34836b7d4087
Umar Niazi <uhkniazi@gmail.com>
2020-01-02 14:46:01 +0000

    reads aligned over a selected region from bam file extracted

--- 18_bam2ExpressedSequence.R
+++ 18_bam2ExpressedSequence.R
@@ -4,24 +4,54 @@
 #       and align those reads with the reference sequence for the gene
 # Date: 20/12/2019
 
+source('header.R')
+library('RMySQL')
 library(TxDb.Mmusculus.UCSC.mm10.knownGene)
 library(BSgenome.Mmusculus.UCSC.mm10)
+library(Biostrings)
+library(GenomicAlignments)
+
+##### connect to mysql database to get samples
+db = dbConnect(MySQL(), user='rstudio', password='12345', dbname='Projects', host='127.0.0.1')
+dbListTables(db)
+dbListFields(db, 'File')
+dbListFields(db, 'Sample')
+# get the query
+g_did
+q = paste0('select Sample.id as sid, Sample.group1, Sample.group2, Sample.title, File.* from Sample, File
+           where (Sample.idData = 41) AND (File.idSample = Sample.id AND File.type like "mcgill quality 10 sorted bam")')
+dfSample = dbGetQuery(db, q)
+nrow(dfSample)
+dfSample
+# close connection after getting data
+dbDisconnect(db)
+# remove any whitespace from the names
+dfSample$name = gsub(" ", "", dfSample$name, fixed = T)
+dfSample$title = gsub(" ", "", dfSample$title, fixed = T)
+i = grep('q10_sort.bam', dfSample$name)
+dfSample = dfSample[i,]
+#### set working directory to appropriate location with bam files
+setwd('dataExternal/bams/')
+csFiles = list.files('.', pattern = '*.bam$', recursive = T)
+# check if these files match the file names in database
+table(dfSample$name %in% csFiles)
+
+dfSample = dfSample[dfSample$name %in% csFiles, ]
+dim(dfSample)
+csFiles = dfSample$name
 
 # enterez id for the gene of interest
-oGRgene = genes(TxDb.Mmusculus.UCSC.mm10.knownGene)
+# reset format to NCBI from UCSC
+tx = TxDb.Mmusculus.UCSC.mm10.knownGene
+seqlevelsStyle(tx)
+seqlevelsStyle(tx) = 'NCBI'
+oGRgene = genes(tx)
 oGRgene = oGRgene['22761']
+oGRgene = GRanges(as.character(seqnames(oGRgene)), ranges(oGRgene), as.character(strand(oGRgene)))
 
-library(GenomicAlignments)
-bam = readGAlignments(file.choose(), param=ScanBamParam(which=oGRgene, what='seq'))
-
-# sequenceLayer
-# s = mcols(bam[1])$seq
-# sequenceLayer(s, cigar(bam)[1])
-#s2 = stackStringsFromBam(file.choose(), param=ScanBamParam(which=oGRgene, what='seq'))
-
-oPile = pileup(file.choose(), scanBamParam = ScanBamParam(which = oGRgene), 
-               pileupParam = PileupParam(distinguish_strands = F, max_depth = 1000, min_base_quality = 30, 
-                                         min_mapq = 30))
+bs = BSgenome.Mmusculus.UCSC.mm10
+seqlevelsStyle(bs) = 'NCBI'
+oDSRef = getSeq(bs, oGRgene)
 
 f_getSeq = function(pile){
   # split the pile up data frame on positions
@@ -46,108 +76,6 @@ f_getSeq = function(pile){
   return(seq)
 }
 
-# getSequenceParameters = function(ivSeq, cRefBase, prior=c(A=1/2, T=1/2, G=1/2, C=1/2), iSize=1000){
-#   if(!require(LearnBayes)) stop('R Package LearnBayes required')
-#   ## internal functions
-#   # get alpha values for dirichlet posterior
-#   getAlpha = function(seq, prior=c(A=1/2, T=1/2, G=1/2, C=1/2)){
-#     #a = letterFrequency(seq, letters='ATGC', OR=0, as.prob=F)
-#     alpha = seq + prior #a + prior
-#     return(alpha)
-#   }
-#   # calculate dirichlet variance 
-#   getDirichletVariance = function(alpha){
-#     al0 = sum(alpha)
-#     denom = al0^2 * (al0+1)
-#     ret = sapply(alpha, function(x){
-#       return(x * (al0 - x) / denom)
-#     })
-#     return(ret)
-#   }
-#   # get posterior theta from posterior dirichlet
-#   getPosterior = function(alpha, n=1000){
-#     p = rdirichlet(n, alpha)
-#     colnames(p) = names(alpha)
-#     #m = colMeans(p)
-#     return(p)
-#   }
-#   
-#   # posterior gamma, a component of the dirichlet distribution
-#   ## see gelman P 583 and bayesian computations with R page 66
-#   getPosteriorGamma = function(alpha.scale, base, n=1000, prior){
-#     # adjust alpha by removing dirichlet prior
-#     alpha.scale = alpha.scale - prior
-#     i = which(names(alpha.scale) == base)
-#     alpha.new = c(alpha.scale[i], sum(alpha.scale[-i]))
-#     names(alpha.new) = c('Base', 'Other')
-#     ## set a non-informative jeffery's prior for gamma distributed rate
-#     jef.prior = c(alpha=0.5, beta=1)
-#     # adjust the new alpha for gamma posterior
-#     alpha.new = alpha.new + jef.prior['alpha']
-#     # convert the alpha to rate per 1000
-#     alpha.new = (alpha.new/sum(alpha.new)) * 1000
-#     rg = sapply(seq_along(alpha.new), function(x) {
-#       return(rgamma(n, alpha.new[x], jef.prior['beta']))
-#     })
-#     colnames(rg) = names(alpha.new)
-#     return(rg)
-#   }
-#   ###### processing steps
-#   ## get posterior values
-#   a = getAlpha(ivSeq, prior)
-#   # get posterior dirichlet variance
-#   var = getDirichletVariance(a)
-#   # get posterior via simulation
-#   p = getPosterior(a, iSize)
-#   var = sum(var)
-#   theta = colMeans(p)[cRefBase]
-#   if (is.na(theta)) {rate=c('Base'= NA, 'Other'=NA) } else {
-#     rate = round(colMeans(getPosteriorGamma(a, cRefBase, iSize, prior)), 2)}
-#   return(c(theta=theta, var=var, lambda.base=rate['Base'], lambda.other=rate['Other'], ivSeq))
-# }
-
-s = f_getSeq(oPile)
-
-oDSRef = getSeq(BSgenome.Mmusculus.UCSC.mm10, oGRgene)
-# param = sapply(rownames(s), function(x){
-#   gr = GRanges(seqnames(oGRgene), ranges = IRanges(start = as.numeric(x), width=1), strand=strand(oGRgene))
-#   ref = getSeq(BSgenome.Mmusculus.UCSC.mm10, gr)
-#   getSequenceParameters(s[x,], as.character(ref[[1]]))
-# })
-# 
-# colnames(param) = rownames(s)
-# rownames(param) = c('theta', 'var', 'lambda.base', 'lambda.other', 'A', 'T', 'G', 'C')
-# param = t(param)
-# dim(param)
-# # create factors for quantiles
-# param = na.omit(param)
-# dim(param)
-# # var.q = cut(param[,'var'],breaks = quantile(param[,'var'], c(0, 0.05, 0.95, 1)), include.lowest = T, 
-# #             labels = c('q.low', 'q,m', 'q.high'))
-# theta.q = cut(param[,'theta'],breaks = quantile(param[,'theta'], c(0, 0.01, 0.95, 1)), include.lowest = T, 
-#               labels = c('q.low', 'q.m', 'q.high'))
-# param = cbind(param, theta.q)
-param = s
-cvConsensus = apply(param[,c('A', 'T', 'G', 'C')], 1, function(x){
-  return(c('A', 'T', 'G', 'C')[which.max(x)])
-})
-param = data.frame(param, consensus=cvConsensus, stringsAsFactors = F)
-## reformat the data matrix 
-dfResult = data.frame(reference=unlist(strsplit(toString(oDSRef[[1]]), split = '')),
-                      row.names=c(start(oGRgene):end(oGRgene)), stringsAsFactors = F)
-m = match(rownames(param), rownames(dfResult))
-dfResult$consensus = '-'
-dfResult$consensus[m] = param$consensus
-
-# cvConsensus = unlist(cvConsensus)
-# dfResult = data.frame(mFile)
-# dfResult$consensus = NA
-# m = match(names(cvConsensus), rownames(mFile))
-# dfResult[m,'consensus'] = cvConsensus
-
-# pat = paste0(cvConsensus, collapse = '')
-# pwa = pairwiseAlignment(pat, oDSRef, type='global')
-
 # NAME: f_oDNAStringSetConvertPWAMatrix
 # ARGS: pwa.matrix = a pairwise alignment object in matrix form, generated via a call like
 #       as.matrix(pairwiseAlignment(patten, subject))
@@ -169,6 +97,37 @@ f_oDNAStringSetConvertPWAMatrix= function(pwa.matrix){
   return(seq.export)
 }
 
-export(f_oDNAStringSetConvertPWAMatrix(t(dfResult)), 'temp/alignment.fasta')
+oPile = lapply(csFiles, function(x) pileup(x, scanBamParam = ScanBamParam(which = oGRgene), 
+               pileupParam = PileupParam(distinguish_strands = F, max_depth = 1000, min_base_quality = 30, 
+                                         min_mapq = 30)))
 
-m = rbind(c, dfResult$consensus)
+lParam = lapply(oPile, f_getSeq)
+
+lConsensus = lapply(lParam, function(param){
+  cvConsensus = apply(param[,c('A', 'T', 'G', 'C')], 1, function(x){
+    return(c('A', 'T', 'G', 'C')[which.max(x)])
+  })
+  names(cvConsensus) = rownames(param)
+  return(cvConsensus)
+})
+
+names(lConsensus) = paste(dfSample$idSample, dfSample$title, sep = '_')
+
+#param = data.frame(param, consensus=cvConsensus, stringsAsFactors = F)
+## reformat the data matrix 
+dfResult = data.frame(reference=unlist(strsplit(toString(oDSRef[[1]]), split = '')),
+                      row.names=c(start(oGRgene):end(oGRgene)), stringsAsFactors = F)
+
+for (i in 1:length(lConsensus)){
+  m = match(names(lConsensus[[i]]), rownames(dfResult))
+  dfResult = cbind(dfResult, '-', stringsAsFactors=F)
+  dfResult[m, i+1] = lConsensus[[i]]
+  colnames(dfResult)[i+1] = names(lConsensus)[i]
+}
+
+## create appropriate file name
+cvExport = paste0('../../results/alignments/', 'all.fasta')
+oDSalignment = f_oDNAStringSetConvertPWAMatrix(t(dfResult))
+export(oDSalignment, cvExport)
+
+
